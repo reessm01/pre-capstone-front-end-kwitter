7cd35cdd376de12df4c7c743ec9743c64d950ae3
Tyler <tymitchell2100@gmail.com>
2019-03-11 19:07:14 -0400

    bootstrap styling and Link between signup/login

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 // import { connect } from "react-redux";
 // import { loginThenGoToUserProfile as login } from "../actions";
 import Spinner from "react-spinkit";
+import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+const cardStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '2rem',
+  width: '20rem'
+}
 
 class SignUp extends Component {
   state = { username: "", password: "", email: "" };
@@ -19,34 +31,56 @@ class SignUp extends Component {
     const { isLoading, err } = this.props;
     return (
       <React.Fragment>
-        <h1>Sign Up</h1>
-        <form onSubmit={this.handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={isLoading}>
-            Submit
-          </button>
-        </form>
+        <Card style={cardStyle}>
+          <Card.Img variant="top" src="./img/kenzieLogo.png" />
+          <Card.Body>
+            <Card.Title>Welcome to Kwitter</Card.Title>
+            <hr />
+            <Form onSubmit={this.handleLogin}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label htmlFor="username">Username</Form.Label>
+                <Form.Control 
+                  type="text" 
+                  placeholder="username"
+                  name="username"
+                  autoFocus
+                  required
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control 
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  required
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label htmlFor="email">email</Form.Label>
+                <Form.Control 
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  required
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button disabled={isLoading} variant="primary" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+            {isLoading && <Spinner name="circle" color="blue" />}
+            {err && <p style={{ color: "red" }}>{err}</p>}
+            <Link to="/">Here to Login</Link>
+          </Card.Body>
+        </Card>
         {isLoading && <Spinner name="circle" color="blue" />}
         {err && <p style={{ color: "red" }}>{err}</p>}
       </React.Fragment>
