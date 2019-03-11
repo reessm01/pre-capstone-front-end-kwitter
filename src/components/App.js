b0280f04879b6cecb793e6075ed633361b9f1582
Tyler <tymitchell2100@gmail.com>
2019-03-11 12:45:50 -0400

    import signup component to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import SignUp from "./SignUp"
 import { LoginForm, UserProfile } from ".";
 
 class App extends Component {
