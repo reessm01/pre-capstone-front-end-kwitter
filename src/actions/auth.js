30bbcf5e8bc20dd320b800d6d332614537a1984f
Scott Reese <scott_reese@pm.me>
2019-03-13 11:51:04 -0400

    semicolons removed - prettier updated to exclude them

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,16 +1,16 @@
-import { domain, jsonHeaders, handleJsonResponse } from "./constants";
-import { push } from "connected-react-router";
+import { domain, jsonHeaders, handleJsonResponse } from "./constants"
+import { push } from "connected-react-router"
 
-export const LOGIN = "LOGIN";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGIN = "LOGIN"
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const LOGIN_FAIL = "LOGIN_FAIL"
 
-const url = domain + "/auth";
+const url = domain + "/auth"
 
 const login = loginData => dispatch => {
   dispatch({
     type: LOGIN
-  });
+  })
 
   return fetch(url + "/login", {
     method: "POST",
@@ -19,20 +19,20 @@ const login = loginData => dispatch => {
   })
     .then(handleJsonResponse)
     .then(result => {
-      console.log(result);
-      localStorage.setItem("token", result.token);
+      console.log(result)
+      localStorage.setItem("token", result.token)
       return dispatch({
         type: LOGIN_SUCCESS,
         payload: result
-      });
+      })
     })
     .catch(err => {
       return Promise.reject(
         dispatch({ type: LOGIN_FAIL, payload: err.message })
-      );
-    });
-};
+      )
+    })
+}
 
 export const loginThenGoToUserProfile = loginData => dispatch => {
-  return dispatch(login(loginData)).then(() => dispatch(push("/home")));
-};
+  return dispatch(login(loginData)).then(() => dispatch(push("/home")))
+}
