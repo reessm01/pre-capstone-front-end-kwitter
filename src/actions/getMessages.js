a520b0a16dbd824d865813f48d8739eb5c284818
Tyler <tymitchell2100@gmail.com>
2019-03-14 11:33:21 -0400

    styling for register/login and timelinepost

--- src/actions/getMessages.js
+++ src/actions/getMessages.js
@@ -18,6 +18,3 @@ export const getMessages = () => dispatch => {
     })
   }
 
-
-  
-  
--- src/components/TimelinePost.js
+++ src/components/TimelinePost.js
@@ -4,10 +4,31 @@ import { domain, handleJsonResponse } from "../actions/constants"
 
 const url = domain + '/users/'
 
+const cardStyle = {
+  width: '18rem'
+}
+
+const postHeaderStyle = {
+  display: 'flex',
+  marginBottom: '1rem'
+}
+
+const cardImgStyle = { 
+  height: '3rem',
+  width: '3rem',
+  marginRight: '1rem'
+}
+
+const namesDivStyle = {
+  display: 'flex',
+  flexDirection: 'column'
+}
+
 export default class TimelinePost extends Component{
   state = {
     displayName: '',
-    username: ''
+    username: '',
+    photoUrl: 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmybroadband.co.za%2Fnews%2Fwp-content%2Fuploads%2F2017%2F04%2FTwitter-profile-picture.jpg&f=1'
   }
 
   componentDidMount() {
@@ -19,16 +40,39 @@ export default class TimelinePost extends Component{
           username: result.user.displayName,
         })
       })
+    
+    fetch(url + this.props.id + '/picture')
+      .then(result => {
+        console.log(result)
+        if(result.status === 200) {
+          this.setState({
+            photoUrl: result.url
+          })
+        }
+      })
   }
 
   render(){
-      return(
-      <Card style={{ width: '18rem' }}>
+    const {
+      displayName,
+      username,
+      photoUrl
+    } = this.state
+
+    const { text } = this.props
+
+    return(
+      <Card style={ cardStyle }>
         <Card.Body>
-          <Card.Title>{this.state.displayName}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">@{this.state.username}</Card.Subtitle>
+          <div style={ postHeaderStyle }>
+            <Card.Img style={ cardImgStyle } src={ photoUrl }></Card.Img>
+            <div style={ namesDivStyle }>
+              <Card.Title>{ displayName }</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">@{ username }</Card.Subtitle>
+            </div>
+          </div>
           <Card.Text>
-            {this.props.text}
+            { text }
           </Card.Text>
           <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link>
