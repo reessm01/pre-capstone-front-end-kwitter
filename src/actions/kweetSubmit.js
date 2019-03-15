4fd8082ad1a249f2f4dcf4f08bee84ece990cf0c
Scott Reese <scott_reese@pm.me>
2019-03-15 13:24:58 -0400

    new kweet error message added

--- src/actions/kweetSubmit.js
+++ src/actions/kweetSubmit.js
@@ -1,23 +1,30 @@
 import { domain, jsonHeaders, handleJsonResponse } from "./constants"
+import { MESSAGES_FAILED } from "../actions/getMessages"
 
 export const NEW_KWEET = "NEW_KWEET"
 
 const url = domain + "/messages/"
 
 export const handleKweetSubmit = kweetData => dispatch => {
-    return fetch(url, {
-        method: "POST",
-        headers: {
-            ...jsonHeaders,
-            Authorization: `Bearer ${kweetData.token}`
-        },
-        body: JSON.stringify( kweetData.text ),
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      ...jsonHeaders,
+      Authorization: `Bearer ${kweetData.token}`
+    },
+    body: JSON.stringify(kweetData.text)
+  })
+    .then(handleJsonResponse)
+    .then(result => {
+      return dispatch({
+        type: NEW_KWEET,
+        payload: result
+      })
     })
-        .then(handleJsonResponse)
-        .then(result => {
-            return dispatch({
-                type: NEW_KWEET,
-                payload: result
-            })
-        })
+    .catch(() => {
+      console.log("working")
+      return dispatch({
+        type: MESSAGES_FAILED
+      })
+    })
 }
