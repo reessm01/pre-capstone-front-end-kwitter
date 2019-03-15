09efe80a84a81144728c57c5d1bd4b282fb6f338
Tyler <tymitchell2100@gmail.com>
2019-03-15 13:33:14 -0400

    user sidebar styling

--- src/components/UsersSidebar.js
+++ src/components/UsersSidebar.js
@@ -3,10 +3,27 @@ import Card from 'react-bootstrap/Card'
 import {getUsers} from '../actions/getUsers'
 import {connect} from 'react-redux'
 
-const cardStyle = {
-    width: '18rem',
-    padding: '2rem',
-    backgroundColor: "#DCDCDC"
+const userCard = {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    padding: '1rem',
+    paddingTop: '1.5rem',
+    paddingLeft: '1.5rem',
+    backgroundColor: "white",
+    borderRadius: '5px'
+}
+
+const cardImgStyle = { 
+    height: '3rem',
+    width: '3rem',
+    marginRight: '1rem'
+}
+
+const namesDivStyle = {
+    display: 'flex',
+    flexDirection: 'column'
 }
 
 export class UsersSidebar extends Component{
@@ -17,16 +34,24 @@ export class UsersSidebar extends Component{
     
     render(){
         const { users } = this.props
-
+        
         const userBodies = users.map(user => (
-            <Card.Body>{user.displayName}</Card.Body>
+            <Card style={userCard }>
+                <Card.Img style={ cardImgStyle } src={ 'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fmybroadband.co.za%2Fnews%2Fwp-content%2Fuploads%2F2017%2F04%2FTwitter-profile-picture.jpg&f=1' }></Card.Img>
+                <div style={ namesDivStyle }>
+                    <Card.Title>{ user.displayName }</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">@{ user.username }</Card.Subtitle>
+                </div>
+            </Card>
         ))
 
         return (
             <React.Fragment>
-                <Card id="show-users" style={cardStyle}>
-                    {userBodies}
-                </Card>
+                <div id="show-users">
+                    <div style={{ height: 'fit-content' }}>
+                        {userBodies}
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
