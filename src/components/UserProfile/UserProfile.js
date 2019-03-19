bac6a6be69fa8f9533f976ead9467921ab47a256
Tyler <tymitchell2100@gmail.com>
2019-03-19 18:23:39 -0400

    adds infinite scrolling

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import { GeneralHeader, ProfileBox, Timeline, UsersSidebar} from '../'
 import {getMessages} from '../../actions/getMessages'
 import {connect} from 'react-redux'
+import { timelineStyle, mainStyle } from './style'
 
  class UserProfile extends Component {
 
@@ -15,11 +16,11 @@ import {connect} from 'react-redux'
         return (
             <React.Fragment>
                 <GeneralHeader />
-                <div id="main-wrap">
+                <div id="main-wrap" style={ mainStyle }>
                     <div className="profile-mobile wrap">
                         <ProfileBox className="mobile" />
                     </div>
-                    <div className="timeline wrap">
+                    <div style={ timelineStyle } className="wrap">
                         <Timeline messages={this.props.messages.filter(message => {
                             return this.props.currentUserId === message.userId
                         })}/>
---
+++ src/components/UserProfile/style.js
@@ -0,0 +1,11 @@
+export const timelineStyle = {
+    width: "32rem",
+    borderWidth: "0px",
+    marginLeft:"15px",
+    marginRight:"15px"
+}
+
+export const mainStyle = {
+    display:"flex",
+    justifyContent:"center"
+}
