5c788e9521b896ea5cf478148d62f1cdae604eb5
Tyler <tymitchell2100@gmail.com>
2019-03-20 10:19:58 -0400

    fade in transitions

--- src/components/ProfileBox/ProfileBox.js
+++ src/components/ProfileBox/ProfileBox.js
@@ -9,6 +9,7 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEdit } from "@fortawesome/free-solid-svg-icons"
 import { cardStyle, cardTitleStyle, cardTextStyle, cardImgStyle } from "./style"
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 library.add(faEdit)
 
@@ -55,64 +56,84 @@ class ProfileBox extends Component {
     const { edit } = this.state
 
     return (
-      <Card style={cardStyle}>
-        <Card.Img style={cardImgStyle} variant="top" src={pic} key={updated} />
-        {edit ? (
-          <Form onSubmit={handleEdit} style={{ marginTop: "30px" }}>
-            <Form.Group controlId="displayName">
-              <Form.Label>Display Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="displayName"
-                placeholder={displayName}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="bio">
-              <Form.Label>Bio</Form.Label>
-              <Form.Control
-                name="about"
-                maxlength="100"
-                as="textarea"
-                rows="3"
-                placeholder={bio}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Picture Upload</Form.Label>
-              <Form.Control
-                name="picture"
-                placeholder="picture"
-                title="picture"
-                type="file"
-              />
-            </Form.Group>
+        <ReactCSSTransitionGroup
+            transitionEnterTimeout={2000}
+            transitionLeaveTimeout={300}
+            transitionName="fade"
+            transitionAppear={true}
+            transitionAppearTimeout={2000}
+            transitionEnter={true}
+            transitionLeave={true}
+        >
+            <Card style={cardStyle}>
+              <ReactCSSTransitionGroup
+                style={cardImgStyle}
+                transitionName="fade"
+                transitionEnter={true}
+                transitionEnterTimeout={2000}
+                transitionLeave={true}
+                transitionLeaveTimeout={300}
+                transitionAppear={false}
+                transitionAppearTimeout={2000}
+              >
+                <Card.Img variant="top" src={pic} key={updated} />
+              </ReactCSSTransitionGroup>
+                {edit ? (
+                  <Form onSubmit={handleEdit} style={{ marginTop: "30px" }}>
+                      <Form.Group controlId="displayName">
+                        <Form.Label>Display Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="displayName"
+                            placeholder={displayName}
+                            onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Group controlId="bio">
+                        <Form.Label>Bio</Form.Label>
+                        <Form.Control
+                            name="about"
+                            maxlength="100"
+                            as="textarea"
+                            rows="3"
+                            placeholder={bio}
+                            onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Group>
+                        <Form.Label>Picture Upload</Form.Label>
+                        <Form.Control
+                            name="picture"
+                            placeholder="picture"
+                            title="picture"
+                            type="file"
+                        />
+                      </Form.Group>
 
-            <Button
-              type="submit"
-              value="Submit"
-              style={{ marginBottom: "30px" }}
-            >
-              Save
-            </Button>
-          </Form>
-          
-        ) : (
-          <Card.Body>
-            <Link to='/userProfile'>
-                <Card.Title style={cardTitleStyle}>{displayName}</Card.Title>
-            </Link>
-            <Link to='/userProfile'>
-                <small>@{username}</small>
-            </Link>
-            <Card.Text style={cardTextStyle}>{bio}</Card.Text>
-            <Button onClick={() => this.setState({ edit: !edit })}>
-              <FontAwesomeIcon icon="edit" />
-            </Button>
-          </Card.Body>
-        )}
-      </Card>
+                      <Button
+                        type="submit"
+                        value="Submit"
+                        style={{ marginBottom: "30px" }}
+                      >
+                        Save
+                      </Button>
+                  </Form>
+                ) : (
+                  <Card.Body>
+                      <Link to='/userProfile'>
+                          <Card.Title style={cardTitleStyle}>{displayName}</Card.Title>
+                      </Link>
+                      <Link to='/userProfile'>
+                          <small>@{username}</small>
+                      </Link>
+                      <Card.Text style={cardTextStyle}>{bio}</Card.Text>
+                      <Button onClick={() => this.setState({ edit: !edit })}>
+                        <FontAwesomeIcon icon="edit" />
+                      </Button>
+                  </Card.Body>
+                )}
+            </Card>
+        </ReactCSSTransitionGroup>
     )
   }
 }
--- src/components/TimelinePost/TimelinePost.js
+++ src/components/TimelinePost/TimelinePost.js
@@ -5,6 +5,7 @@ import { domain, handleJsonResponse } from "../../actions/constants"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart } from "@fortawesome/free-solid-svg-icons"
+
 import {
   cardStyle,
   postHeaderStyle,
