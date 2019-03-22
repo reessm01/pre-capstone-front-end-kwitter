27e954b8928338be3c14f01a87a3468dbd160301
Nick D <nmdanner123@gmail.com>
2019-03-22 16:55:13 -0400

    fixing router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css"
 import { App } from "./components"
 import * as serviceWorker from "./serviceWorker"
 import { Provider } from "react-redux"
+import {BrowserRouter} from 'react-router-dom'
 import { ConnectedRouter } from "connected-react-router"
 import { Route } from "react-router-dom"
 import configureStore, { history } from "./configureStore"
@@ -12,9 +13,9 @@ export const store = configureStore({})
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
+    <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
       <Route component={App} />
-    </ConnectedRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 )
