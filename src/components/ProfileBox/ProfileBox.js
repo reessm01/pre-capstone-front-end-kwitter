487db47f2ed65c2cd05af23fccefd72008a0ea6b
Scott Reese <scott_reese@pm.me>
2019-03-22 11:46:37 -0400

    button styles are now consistent

--- src/components/ProfileBox/ProfileBox.js
+++ src/components/ProfileBox/ProfileBox.js
@@ -15,7 +15,8 @@ import {
   cardStyle,
   cardTitleStyle,
   cardTextStyle,
-  cardImgStyle
+  cardImgStyle,
+  buttonStyle
 } from "./style"
 
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
@@ -56,84 +57,84 @@ class ProfileBox extends Component {
     const { edit } = this.state
 
     return (
-        <ReactCSSTransitionGroup
-            transitionEnterTimeout={2000}
-            transitionLeaveTimeout={300}
+      <ReactCSSTransitionGroup
+        transitionEnterTimeout={2000}
+        transitionLeaveTimeout={300}
+        transitionName="fade"
+        transitionAppear={true}
+        transitionAppearTimeout={2000}
+        transitionEnter={true}
+        transitionLeave={true}
+      >
+        <Card style={cardStyle}>
+          <ReactCSSTransitionGroup
+            style={cardImgStyle}
             transitionName="fade"
-            transitionAppear={true}
-            transitionAppearTimeout={2000}
             transitionEnter={true}
+            transitionEnterTimeout={2000}
             transitionLeave={true}
-        >
-            <Card style={cardStyle}>
-              <ReactCSSTransitionGroup
-                style={cardImgStyle}
-                transitionName="fade"
-                transitionEnter={true}
-                transitionEnterTimeout={2000}
-                transitionLeave={true}
-                transitionLeaveTimeout={300}
-                transitionAppear={false}
-                transitionAppearTimeout={2000}
-              >
-                <Card.Img variant="top" src={pic} key={updated} />
-              </ReactCSSTransitionGroup>
-                {edit ? (
-                  <Form onSubmit={handleEdit} style={{ marginTop: "30px" }}>
-                      <Form.Group controlId="displayName">
-                        <Form.Label>Display Name</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="displayName"
-                            placeholder={displayName}
-                            onChange={handleChange}
-                        />
-                      </Form.Group>
-                      <Form.Group controlId="bio">
-                        <Form.Label>Bio</Form.Label>
-                        <Form.Control
-                            name="about"
-                            maxlength="100"
-                            as="textarea"
-                            rows="3"
-                            placeholder={bio}
-                            onChange={handleChange}
-                        />
-                      </Form.Group>
-                      <Form.Group>
-                        <Form.Label>Picture Upload</Form.Label>
-                        <Form.Control
-                            name="picture"
-                            placeholder="picture"
-                            title="picture"
-                            type="file"
-                        />
-                      </Form.Group>
+            transitionLeaveTimeout={300}
+            transitionAppear={false}
+            transitionAppearTimeout={2000}
+          >
+            <Card.Img variant="top" src={pic} key={updated} />
+          </ReactCSSTransitionGroup>
+          {edit ? (
+            <Form onSubmit={handleEdit} style={{ marginTop: "30px" }}>
+              <Form.Group controlId="displayName">
+                <Form.Label>Display Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="displayName"
+                  placeholder={displayName}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="bio">
+                <Form.Label>Bio</Form.Label>
+                <Form.Control
+                  name="about"
+                  maxlength="100"
+                  as="textarea"
+                  rows="3"
+                  placeholder={bio}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Picture Upload</Form.Label>
+                <Form.Control
+                  name="picture"
+                  placeholder="picture"
+                  title="picture"
+                  type="file"
+                />
+              </Form.Group>
 
-                      <Button
-                        type="submit"
-                        value="Submit"
-                        style={{ marginBottom: "30px" }}
-                      >
-                        Save
+              <Button
+                type="submit"
+                value="Submit"
+                style={buttonStyle}
+              >
+                Save
                       </Button>
-                  </Form>
-                ) : (
-                  <Card.Body>
-                      <Link to='/userProfile'>
-                          <Card.Title style={ cardTitleStyle }>{ displayName }</Card.Title>
-                      </Link>
-                      <Link to='/userProfile'>
-                          <small>@{ username }</small>
-                      </Link>
-                      <Card.Text style={ cardTextStyle }>{ bio }</Card.Text>
-                      <Button onClick={() => this.setState({ edit: !edit })}>
-                        <FontAwesomeIcon icon="edit" />
-                      </Button>
-                  </Card.Body>
-                )}
-            </Card>
-        </ReactCSSTransitionGroup>
+            </Form>
+          ) : (
+              <Card.Body>
+                <Link to='/userProfile'>
+                  <Card.Title style={cardTitleStyle}>{displayName}</Card.Title>
+                </Link>
+                <Link to='/userProfile'>
+                  <small>@{username}</small>
+                </Link>
+                <Card.Text style={cardTextStyle}>{bio}</Card.Text>
+                <Button style={buttonStyle} onClick={() => this.setState({ edit: !edit })}>
+                  <FontAwesomeIcon icon="edit" />
+                </Button>
+              </Card.Body>
+            )}
+        </Card>
+      </ReactCSSTransitionGroup>
     )
   }
 }
