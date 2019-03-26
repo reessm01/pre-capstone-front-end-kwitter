ac4b1751214a80f628e3f27a7663cdee09656709
Scott Reese <scott_reese@pm.me>
2019-03-25 21:51:53 -0400

    added switches

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from "./components"
 import * as serviceWorker from "./serviceWorker"
 import { Provider } from "react-redux"
 import { ConnectedRouter } from 'connected-react-router'
-import { Route } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import configureStore, { history } from "./configureStore"
 
 export const store = configureStore({})
@@ -13,7 +13,9 @@ export const store = configureStore({})
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
-      <Route component={App} />
+      <Switch>
+        <Route component={App} />
+      </Switch>
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
